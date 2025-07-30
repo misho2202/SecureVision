6f0e72678decd865a8d6bcad8c8a17ccfe962a48
kali linux <kali@kali.linux.com>
2025-07-30 02:39:06 -0400

    Removed unnecessary files and codes

--- media_scanner/urls.py
+++ media_scanner/urls.py
@@ -2,13 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path("upload/", views.upload, name="upload"),
-    # path("upload/", views.upload_and_process, name="upload"),
-    # path("livestream/", views.livestream_view, name="livestream"),
-    # path("livestream/", views.livestream_feed, name="livestream"),
     path("livestream/disconnect/", views.disconnect_livestream, name="disconnect_livestream"),
-    # path('uploaded-images/', views.uploaded_images, name='uploaded_images'),
-    # path('delete-image/', views.delete_image, name='delete_image'),  # single
-    # path('delete-all-images/', views.delete_all_images, name='delete_all'),  # all
 ]
